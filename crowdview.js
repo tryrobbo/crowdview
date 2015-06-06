d98da17c33880c9ab3afbae11eac99aacfbc84ff
Thomas Robinson <trobinson@adaptavist.com>
2015-06-06 17:21:15 +0100

    With table of results

--- crowdview.js
+++ crowdview.js
@@ -2,20 +2,13 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-   
-
+    
+    Template.listWatchSessions.helpers({
+  WSList: function() {
+    return WatchSessions.find();
+  }
+})
+    
 }
 
 if (Meteor.isServer) {
@@ -44,3 +37,6 @@ WatchSessions.attachSchema(new SimpleSchema({
     
   }
 ));
+
+SimpleSchema.debug = true
+
